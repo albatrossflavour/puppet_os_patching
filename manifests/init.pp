7e55f941b0a3b3802e1afbca1a314ce88cb5ca32
Tony Green <tgreen@bandcamp.tv>
2018-05-24 14:25:57 +1000

    Manage the cron job

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,22 @@
+# @summary We'll get to the doco soon
+# 
 class os_patching (
   $patch_data_bin_dir    = '/usr/local/bin',
   $patch_data_cache_dir  = '/var/patching-data',
   $patch_data_owner      = 'root',
   $patch_data_group      = 'root',
   $patch_required_pkgs   = [ 'yum', 'rpm' ],
+  $patch_cron_hour       = '*',
+  $patch_cron_min        = fqdn_rand(59),
+  $patch_cron_user       = $patch_data_owner,
 ){
-
   $cache_cmd =  "${patch_data_bin_dir}/generate_patch_cache"
+
+  File {
+    owner   => $patch_data_owner,
+    group   => $patch_data_group,
+    mode    => '0700',
+  }
 
   packge { $patch_required_pkgs:
     ensure => present,
@@ -14,26 +24,20 @@ class os_patching (
 
   file { $patch_data_cache_dir:
     ensure  => directory,
-    uid     => $patch_data_owner,
-    gid     => $patch_data_group,
-    mode    => '0700'
-    require => $patch_required_pkgs,
+    require => Package[$patch_required_pkgs],
   }
 
   file { $cache_cmd:
     ensure  => present,
-    uid     => $patch_data_owner
-    gid     => $patch_data_group,
-    mode    => '0700',
-    content => template('profiles/os_patching/generate_patch_cache.erb')
+    content => template('profiles/os_patching/generate_patch_cache.erb'),
   }
 
   cron { 'Cache patching data':
     ensure  => present,
     command => $cache_cmd,
-    user    => 'root',
-    hour    => '*',
-    minute  => fqdn_rand(59),
-    require => File($cache_cmd)
+    user    => $patch_cron_user,
+    hour    => $patch_cron_hour,
+    minute  => $patch_cron_min,
+    require => File($cache_cmd),
   }
 }
