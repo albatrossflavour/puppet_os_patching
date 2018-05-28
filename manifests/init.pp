a814c575a1ca7ffeaab6d2baeb085b9dc4f3d99d
Tony Green <tgreen@bandcamp.tv>
2018-05-29 06:06:43 +1000

    Clean up puppet code

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,14 @@
-# @summary We'll get to the doco soon
- 
+# @summary This manifest sets up the script and cron job to write
+#   custom structured facts that contain patching data.
+# @example include os_patching
 class os_patching (
-  $patch_data_bin_dir    = '/usr/local/bin',
-  $patch_data_owner      = 'root',
-  $patch_data_group      = 'root',
-  $patch_cron_hour       = '*',
-  $patch_cron_min        = fqdn_rand(59),
-  $patch_cron_user       = $patch_data_owner,
-  $install_delta_rpm     = false,
+  String $patch_data_bin_dir    = '/usr/local/bin',
+  String $patch_data_owner      = 'root',
+  String $patch_data_group      = 'root',
+  String $patch_cron_hour       = '*',
+  String $patch_cron_min        = fqdn_rand(59),
+  String $patch_cron_user       = $patch_data_owner,
+  Boolean $install_delta_rpm    = false,
 ){
   $fact_cmd =  "${patch_data_bin_dir}/os_patching_fact_generation.sh"
 
@@ -15,14 +16,11 @@ class os_patching (
     ensure => $install_delta_rpm,
   }
 
-  File {
+  file { $fact_cmd:
+    ensure => present,
     owner   => $patch_data_owner,
     group   => $patch_data_group,
     mode    => '0700',
-  }
-
-  file { $fact_cmd:
-    ensure => present,
     source => "puppet:///modules/${module_name}/os_patching_fact_generation.sh",
   }
 
