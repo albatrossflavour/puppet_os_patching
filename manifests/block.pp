99491920db377102b8f41acdbaff22be26621c1d
Tony Green <tgreen@bandcamp.tv>
2018-06-20 10:47:41 +1000

    todo and blocking

---
+++ manifests/block.pp
@@ -0,0 +1,20 @@
+# @summary Block os_patching tasks from being run on a node
+class os_patching::block (
+  Boolean $block_patching = false,
+  String $block_reason = undef,
+){
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+
+  file { '/etc/os_patching':
+    ensure  => directory,
+  }
+
+  file { '/etc/os_patching/block.conf':
+    ensure  => file,
+    content => template("puppet:///modules/${module_name}/block.conf.erb"),
+  }
+}
