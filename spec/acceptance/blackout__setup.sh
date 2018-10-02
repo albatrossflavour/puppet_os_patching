513e9b890f0b52d5e0b4a777a05e5988182bb833
Tony Green <tgreen@bandcamp.tv>
2018-10-02 14:18:29 +1000

    fix iso fun and games

--- spec/acceptance/blackout__setup.sh
+++ spec/acceptance/blackout__setup.sh
@@ -3,5 +3,5 @@ sh /testcase/spec/acceptance/setup.sh
 puppet apply /testcase/examples/init.pp
 # mark the next ~day as a blackout
 cat << END > /var/cache/os_patching/blackout_windows
-test blackout,$(date --iso-8601=seconds),$(date --iso-8601=seconds --date  "09:00 tomorrow")
+test blackout,$(date --iso-8601=seconds | sed 's/\d(\d{,2})$/0:$1/'),$(date --iso-8601=seconds --date  "09:00 tomorrow" | sed 's/\d(\d{,2})$/0:$1/')
 END
