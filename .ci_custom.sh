1efda613c961600fe29cb38d5c040f73a179758e
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-10-05 21:41:55 +1000

    add missing ci file

---
+++ .ci_custom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# *File originally created by PDQTest*
+# This is your place to add custom CI actions without modifying travis.yml
+# PDQTest will create this initial file for you but only so you know where
+# add your stuff. We won't change this file unless you run `pdqtest init`
+# again
