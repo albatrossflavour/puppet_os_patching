31709404fac4798c207bfba7c4ed0e94d67f1fc8
Nathan Giuliani <nathan.giuliani@nec.com.au>
2019-04-30 20:59:05 +0930

    mod for windows

--- tasks/refresh_fact.rb
+++ tasks/refresh_fact.rb
@@ -1,21 +1,78 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 
-require 'rbconfig'
-is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
-if is_windows
-  puts 'Cannot run os_patching::refresh_fact on Windows'
-  exit 1
+# windows logging class
+class WinLog
+  def initialize
+    require 'win32/eventlog'
+
+    # log to send events to
+    windows_log = 'Application'
+
+    # source of event shown in event log
+    @event_source = 'os_patching'
+
+    # add event source if needed
+    # we probably should generate and register an mc file, but the events still show without it
+    Win32::EventLog.add_event_source(:source => windows_log, :key_name => @event_source)
+
+    # create logger
+    @logger = Win32::EventLog.new
+  end
+
+  # match SysLog::Logger event types
+
+  def debug(data)
+    @logger.report_event(:event_type => Win32::EventLog::INFO_TYPE, :data => "Debug: #{data}", :source => @event_source)
+  end
+
+  def error(data)
+    @logger.report_event(:event_type => Win32::EventLog::ERROR_TYPE, :data => data, :source => @event_source)
+  end
+
+  def fatal(data)
+    @logger.report_event(:event_type => Win32::EventLog::ERROR_TYPE, :data => "FATAL: #{data}", :source => @event_source)
+  end
+
+  def info(data)
+    @logger.report_event(:event_type => Win32::EventLog::INFO_TYPE, :data => data, :source => @event_source)
+  end
+
+  def unknown(data)
+    @logger.report_event(:event_type => Win32::EventLog::INFO_TYPE, :data => "Unknown: #{data}", :source => @event_source)
+  end
+
+  def warn(data)
+    @logger.report_event(:event_type => Win32::EventLog::WARN_TYPE, :data => data, :source => @event_source)
+  end
 end
 
+require 'rbconfig'
 require 'open3'
 require 'json'
-require 'syslog/logger'
 require 'time'
 require 'timeout'
 
+is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
+
 $stdout.sync = true
 
-log = Syslog::Logger.new 'os_patching'
+if is_windows
+  # windows
+  # create windows event logger
+  log = WinLog.new
+  # set paths/commands for windows
+  fact_generation_script = 'C:/ProgramData/os_patching/os_patching_windows.ps1'
+  fact_generation_cmd = "#{ENV['systemroot']}/system32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy RemoteSigned -file #{fact_generation_script}"
+else
+  # not windows
+  # create syslog logger
+  require 'syslog/logger'
+  log = Syslog::Logger.new 'os_patching'
+  # set paths/commands for linux
+  fact_generation_script = '/usr/local/bin/os_patching_fact_generation.sh'
+  fact_generation_cmd = fact_generation_script
+end
+
 starttime = Time.now.iso8601
 BUFFER_SIZE = 4096
 
@@ -50,13 +107,25 @@ def err(code, kind, message, starttime)
   puts JSON.pretty_generate(json)
   shortmsg = message.split("\n").first.chomp
   history(starttime, shortmsg, exitcode, '', '', '')
-  syslog = Syslog::Logger.new 'os_patching'
-  syslog.error "ERROR : #{kind} : #{exitcode} : #{message}"
+  log = if is_windows
+          WinLog.new
+        else
+          Syslog::Logger.new 'os_patching'
+        end
+  log.error "ERROR : #{kind} : #{exitcode} : #{message}"
   exit(exitcode.to_i)
 end
 
 # Update the fact cache
-clean_out, stderr, status = Open3.capture3('/usr/local/bin/os_patching_fact_generation.sh')
+refresh_out, stderr, status = Open3.capture3(fact_generation_cmd)
+
+# make output more readable if on windows
+refresh_out_log = if is_windows
+                    refresh_out.split("\n")
+                  else
+                    refresh_out
+                  end
+
 err(status, 'os_patching/fact_cache_update', stderr, starttime) if status != 0
-output(status, 'Patching fact cache updated', clean_out, starttime)
+output(status, 'Patching fact cache updated', refresh_out_log, starttime)
 log.info 'Patching fact cache updated'
