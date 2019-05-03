434bff1423932e6a075997c3385fa586eaa9c3f3
Tony Green <tgreen@bandcamp.tv>
2019-05-03 10:42:02 +1000

    Implement a minimum coverage level for rspec and tidy up some of the litmus tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,7 +38,7 @@ RSpec.configure do |c|
   end
   c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
   c.after(:suite) do
-    RSpec::Puppet::Coverage.report!(0)
+    RSpec::Puppet::Coverage.report!(90)
   end
 end
 
