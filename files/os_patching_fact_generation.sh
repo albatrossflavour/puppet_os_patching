c225015f0f098e10b72f65e671a4bda92b56216a
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-10-15 06:53:55 +0200

    clear out 'Obsoleting' entries (#158)
    `yum --check-update` will include a list of obsoleting packages if the available update will cause packages to be obsoleted.
    This output needs to be removed from the fact as it causes unwanted behavior.

--- files/os_patching_fact_generation.sh
+++ files/os_patching_fact_generation.sh
@@ -27,7 +27,9 @@ case $(facter osfamily) in
     # We need to filter those out as they screw up the package listing
     FILTER='egrep -v "^Security:"'
     PKGS=$(yum -q check-update 2>/dev/null| $FILTER | egrep -v "is broken|^Loaded plugins" | awk '/^[[:alnum:]]/ {print $1}')
+    PKGS=$(echo $PKGS | sed 's/Obsoleting.*//')
     SECPKGS=$(yum -q --security check-update 2>/dev/null| $FILTER | egrep -v "is broken|^Loaded plugins" | awk '/^[[:alnum:]]/ {print $1}')
+    SECPKGS=$(echo $SECPKGS | sed 's/Obsoleting.*//')
     HELDPKGS=$([ -r /etc/yum/pluginconf.d/versionlock.list ] && awk -F':' '/:/ {print $2}' /etc/yum/pluginconf.d/versionlock.list | sed 's/-[0-9].*//'
 )
   ;;
