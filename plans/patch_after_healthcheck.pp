7ae06140e579ddbe074447cda56dc06df0cb86dc
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-11-22 13:41:33 +0100

    Give plan control over noop_state and runinterval

--- plans/patch_after_healthcheck.pp
+++ plans/patch_after_healthcheck.pp
@@ -2,15 +2,19 @@
 #   [puppet health check](https://forge.puppet.com/albatrossflavour/puppet_health_check)
 #   module to perform a pre-check on the nodes you're planning to patch.  If the nodes pass the
 #   check, they get patched
-plan os_patching::patch_after_healthcheck (TargetSpec $nodes) {
+plan os_patching::patch_after_healthcheck (
+  TargetSpec $nodes
+  Optional[Boolean] $noop_state = false
+  Optional[Integer] $runinterval = 1800
+  ) {  
   # Run an initial health check to make sure the target nodes are ready
 
   $health_checks = run_task('puppet_health_check::agent_health',
                             $nodes,
-                            target_noop_state      => false,
+                            target_noop_state      => $noop_state,
                             target_service_enabled => true,
                             target_service_running => true,
-                            target_runinterval     => 14400,
+                            target_runinterval     => $runinterval,
                             '_catch_errors'        => true,
   )
 
