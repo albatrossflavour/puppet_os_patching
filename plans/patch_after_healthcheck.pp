5e4e7f6330b23e3535752fbe15bb6bd6ced8aa84
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-11-25 00:05:57 +0100

    Improve os_patching::patch_after_healthcheck plan (#167)
    * Update patch_after_healthcheck.pp
    
    * Replace notice() with out::message()
    
    * Give plan control over noop_state and runinterval
    
    * Syntax correction
    
    Forgot commas in plan parameter definitions

--- plans/patch_after_healthcheck.pp
+++ plans/patch_after_healthcheck.pp
@@ -2,15 +2,19 @@
 #   [puppet health check](https://forge.puppet.com/albatrossflavour/puppet_health_check)
 #   module to perform a pre-check on the nodes you're planning to patch.  If the nodes pass the
 #   check, they get patched
-plan os_patching::patch_after_healthcheck (TargetSpec $nodes) {
+plan os_patching::patch_after_healthcheck (
+  TargetSpec $nodes,
+  Optional[Boolean] $noop_state = false,
+  Optional[Integer] $runinterval = 1800,
+  ) {  
   # Run an initial health check to make sure the target nodes are ready
 
   $health_checks = run_task('puppet_health_check::agent_health',
                             $nodes,
-                            target_noop_state      => false,
+                            target_noop_state      => $noop_state,
                             target_service_enabled => true,
                             target_service_running => true,
-                            target_runinterval     => 1800,
+                            target_runinterval     => $runinterval,
                             '_catch_errors'        => true,
   )
 
@@ -20,6 +24,6 @@ plan os_patching::patch_after_healthcheck (TargetSpec $nodes) {
   $skipped_targets = $nodes_skipped.map | $value | { $value['certname'] }
   $targets = $nodes_to_patch.map | $value | { $value['certname'] }
 
-  notice ("Skipping the following nodes due to health check failures : ${nodes_skipped}")
+  out::message("Skipping the following nodes due to health check failures : ${nodes_skipped}")
   return run_task('os_patching::patch_server', $targets)
 }
