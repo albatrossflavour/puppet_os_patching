72e05684fc440b9011104230bbd41cd6371d4a95
Daniel Goulder <A11974A@adia.ae>
2020-02-18 23:59:22 +0400

    ensure that patching file is delivered on windows

--- manifests/init.pp
+++ manifests/init.pp
@@ -380,6 +380,15 @@ class os_patching (
         }
       }
 
+      file { "${cache_dir}/files":
+        ensure => $ensure_dir,
+      }
+      file { "${cache_dir}/files/os_patching_windows.ps1":
+        ensure => $ensure_file,
+        mode   => $fact_mode,
+        source => "puppet://modules/${module_name}/os_patching_windows.ps1",
+      }
+
       scheduled_task { 'os_patching fact generation':
         ensure    => $ensure,
         enabled   => true,
