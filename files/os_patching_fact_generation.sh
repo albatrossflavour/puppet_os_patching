401402d5f4c0ebd946dd99da41ba16f16196a196
Pat Riehecky <riehecky@fnal.gov>
2022-10-05 11:13:38 -0500

    Don't record note from subscription-manager if enabled

--- files/os_patching_fact_generation.sh
+++ files/os_patching_fact_generation.sh
@@ -133,10 +133,10 @@ then
       else
         echo "false" > $DATADIR/reboot_required
       fi
-      /usr/bin/needs-restarting 2>/dev/null | sed 's/[[:space:]]*$//' >$DATADIR/apps_to_restart
+      /usr/bin/needs-restarting 2>/dev/null | grep -v 'Updating Subscription Management repositories' | sed 's/[[:space:]]*$//' >$DATADIR/apps_to_restart
     ;;
     6)
-      /usr/bin/needs-restarting 2>/dev/null 1>$DATADIR/apps_to_restart
+      /usr/bin/needs-restarting 2>/dev/null | grep -v 'Updating Subscription Management repositories' > $DATADIR/apps_to_restart
       if [ $? -gt 0 ]
       then
         echo "true" > $DATADIR/reboot_required
