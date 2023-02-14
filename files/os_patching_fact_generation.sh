dabeb301ee003e1992d074bc3f19b2506c48aae5
Pat Riehecky <3534830+jcpunk@users.noreply.github.com>
2023-02-14 06:55:54 -0600

    Don't record note from subscription-manager if enabled (#221)
    Co-authored-by: Tony Green <tgreen@albatrossflavour.com>

--- files/os_patching_fact_generation.sh
+++ files/os_patching_fact_generation.sh
@@ -134,9 +134,9 @@ then
     else
       echo "false" > $DATADIR/reboot_required
     fi
-    /usr/bin/needs-restarting 2>/dev/null | sed 's/[[:space:]]*$//' >$DATADIR/apps_to_restart
+    /usr/bin/needs-restarting 2>/dev/null | grep -v 'Updating Subscription Management repositories' | sed 's/[[:space:]]*$//' >$DATADIR/apps_to_restart
   else
-    /usr/bin/needs-restarting 2>/dev/null 1>$DATADIR/apps_to_restart
+    /usr/bin/needs-restarting 2>/dev/null | grep -v 'Updating Subscription Management repositories' > $DATADIR/apps_to_restart
     if [ $? -gt 0 ]
     then
       echo "true" > $DATADIR/reboot_required
