84e0a9261244fdb144c32cede587ccdc9638960b
Tony Green <tgreen@albatrossflavour.com>
2023-02-14 23:36:06 +1100

    #217 add an apt update as part of the fact generation (#224)

--- files/os_patching_fact_generation.sh
+++ files/os_patching_fact_generation.sh
@@ -47,6 +47,7 @@ case $OSFAMILY in
     HELDPKGS=$(zypper --non-interactive --no-abbrev --quiet ll | grep '|' | grep -v '^Repository' | awk -F'|' '/^[[:alnum:]]/ {print $2}' | sed 's/^\s*\|\s*$//')
   ;;
   Debian)
+    apt update 2>/dev/null 1>/dev/null
     PKGS=$(apt upgrade -s 2>/dev/null | awk '$1 == "Inst" {print $2}')
     SECPKGS=$(apt upgrade -s 2>/dev/null | awk '$1 == "Inst" && /Security/ {print $2}')
     HELDPKGS=$(dpkg --get-selections | awk '$2 == "hold" {print $1}')
